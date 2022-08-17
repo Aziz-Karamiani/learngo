34cd9fcd32a0d5db8e12d3ce5c880151e37b4fb4
a.karamiani <aziz.karamiani@gmail.com>
2022-08-18 01:07:03 +0430

    Working with IO reader

--- 01-HelloWorld/main.go
+++ 01-HelloWorld/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/Aziz-Karamiani/Udemy_Learn_Go_for_Beginners_Crash_Course_Golang/01-HelloWorld/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
-	var whatToSay string
-	whatToSay = doctor.Intro()
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		}
+
+		fmt.Println(doctor.Response(userInput))
+	}
 }
