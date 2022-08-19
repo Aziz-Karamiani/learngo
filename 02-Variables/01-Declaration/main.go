49469b95349dfa3a741f59b76586602984629ed9
a.karamiani <aziz.karamiani@gmail.com>
2022-08-20 00:09:35 +0430

    Variables Declaration

---
+++ 02-Variables/01-Declaration/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Two way declaration (declare + assign value)
+	var firstNumber int
+	firstNumber = 1
+
+	// One way
+	var secondNumber = 2
+
+	// Shorthand Declaration
+	thirdNumber := 3
+
+	fmt.Println(firstNumber, secondNumber, thirdNumber)
+}
